cba3bdba604980636fc1736f62f854952a888abc
Alexander <akochnev66@gmail.com>
2025-11-02 02:43:35 +0300

    category, country, customer, food router clean

--- app/support/food/router.py
+++ app/support/food/router.py
@@ -5,26 +5,15 @@ from fastapi import Depends
 from app.core.config.database.db_async import get_db
 from app.core.routers.base import BaseRouter
 from app.support.food.model import Food
-from app.support.food.repository import FoodRepository
 from app.support.food.schemas import (FoodRead, FoodCreate, FoodUpdate, FoodCreateRelation,
                                       FoodCreateResponseSchema)
-
-from app.support.food.service import FoodService
 
 
 class FoodRouter(BaseRouter):
     def __init__(self):
         super().__init__(
             model=Food,
-            repo=FoodRepository,
-            create_schema=FoodCreate,
-            read_schema=FoodRead,
-            path_schema=FoodUpdate,
             prefix="/foods",
-            tags=["foods"],
-            service=FoodService,
-            create_schema_relation=FoodCreateRelation,
-            create_response_schema=FoodCreateResponseSchema
         )
 
     async def create(self, data: FoodCreate,
