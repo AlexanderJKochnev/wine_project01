887ad5be2241b48468a9b0cf7178ff351cae3df0
Alexander <akochnev66@gmail.com>
2025-11-02 02:48:23 +0300

    category, country, customer, food, region, subcategory router clean

--- app/support/subcategory/router.py
+++ app/support/subcategory/router.py
@@ -5,25 +5,15 @@ from fastapi import Depends
 from app.core.config.database.db_async import get_db
 from app.core.routers.base import BaseRouter
 from app.support.subcategory.model import Subcategory
-from app.support.subcategory.repository import SubcategoryRepository
 from app.support.subcategory.schemas import (SubcategoryRead, SubcategoryCreate, SubcategoryUpdate,
                                              SubcategoryCreateRelation, SubcategoryCreateResponseSchema)
-from app.support.subcategory.service import SubcategoryService
 
 
 class SubcategoryRouter(BaseRouter):
     def __init__(self):
         super().__init__(
             model=Subcategory,
-            repo=SubcategoryRepository,
-            create_schema=SubcategoryCreate,
-            read_schema=SubcategoryRead,
-            path_schema=SubcategoryUpdate,
-            create_schema_relation=SubcategoryCreateRelation,
-            create_response_schema=SubcategoryCreateResponseSchema,
             prefix="/subcategories",
-            tags=["subcategories"],
-            service=SubcategoryService
         )
 
     async def create(self, data: SubcategoryCreate,
