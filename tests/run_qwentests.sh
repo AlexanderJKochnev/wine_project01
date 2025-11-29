a2e731c9a5654925e4ec0896c0782acbed81ae8b
Alexander <akochnev66@gmail.com>
2025-11-29 08:00:10 +0300

    Текущая ветка: tmp Изменения, которые будут включены в коммит: 	изменено:      .gitignore  	новый файл:    tests/run_qwentests.sh  	новый файл:    tests/test_images/-M-HFvvZiJ559JZ_rOxt.png  	новый файл:    tests/test_images/-M-u-64aYgSWWjuvtEa-.png  	новый файл:    tests/test_images/-OIZxRs3eb_EjmGdstRf.png  	новый файл:    tests/test_images/-OIbS7K9nu7iw-VcdHaO.png  	новый файл:    tests/test_images/-OIbYNctD-egrepFz4ER.png

---
+++ tests/run_qwentests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pytest tests/test_additional_components.py \
+       tests/test_missing_components.py \
+       tests/test_repository_unit.py \
+       tests/test_user_repository_unit.py
