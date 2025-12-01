87327837e67fc0eb6ef199e26b47eb0a454745be
Alexander <akochnev66@gmail.com>
2025-12-01 20:10:53 +0300

    reststart_arwq.sh added

---
+++ restart_arq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# перезапуск arq-worker
+
+set -e
+
+# echo "🔁 Останавливаем arq-worker..."
+# docker-compose stop arq-worker
+
+# echo "🧹 Удаляем старый контейнер (если есть)..."
+# docker-compose rm -f arq-worker
+
+# echo "🚀 Пересобираем образ (опционально)..."
+# docker-compose build --no-cache arq-worker
+
+echo "▶️ Запускаем arq-worker..."
+docker-compose up -d arq-worker
+
+echo "✅ arq-worker перезапущен!"
+docker-compose logs arq-worker
+# запуск со слежением
+# docker-compose logs -f arq-worker
