e660707a67d3fe63f79d8f645d827f8ab1156286
Alexander <akochnev66@gmail.com>
2025-12-06 03:00:03 +0300

    preact troubleshooting

---
+++ preact_example/First Preact Project_files/main.tsx
@@ -0,0 +1,12 @@
+var _jsxFileName = "/app/src/main.tsx";
+import { render } from "/node_modules/.vite/deps/preact.js?v=66b2fcf5";
+import { App } from "/src/App.tsx";
+import "/src/style.css";
+import { jsxDEV as _jsxDEV } from "/node_modules/.vite/deps/preact_jsx-dev-runtime.js?v=66b2fcf5";
+render(_jsxDEV(App, {}, void 0, false, {
+  fileName: _jsxFileName,
+  lineNumber: 6,
+  columnNumber: 8
+}, this), document.getElementById("app"));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IjtBQUNBLFNBQVNBLGNBQWM7QUFDdkIsU0FBU0MsV0FBVztBQUNwQixPQUFPO0FBQWUsU0FBQUMsVUFBQUMsZUFBQTtBQUV0QkgsT0FBT0csUUFBQ0YsS0FBRztBQUFBLEVBQUFHLFVBQUFDO0FBQUFBLEVBQUFDLFlBQUE7QUFBQSxFQUFBQyxjQUFBO0FBQUEsT0FBRSxHQUFHQyxTQUFTQyxlQUFlLEtBQUssQ0FBRSIsIm5hbWVzIjpbInJlbmRlciIsIkFwcCIsImpzeERFViIsIl9qc3hERVYiLCJmaWxlTmFtZSIsIl9qc3hGaWxlTmFtZSIsImxpbmVOdW1iZXIiLCJjb2x1bW5OdW1iZXIiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIl0sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VzIjpbIm1haW4udHN4Il0sInNvdXJjZXNDb250ZW50IjpbIi8vIHNyYy9tYWluLnRzeFxuaW1wb3J0IHsgcmVuZGVyIH0gZnJvbSAncHJlYWN0JztcbmltcG9ydCB7IEFwcCB9IGZyb20gJy4vQXBwJztcbmltcG9ydCAnLi9zdHlsZS5jc3MnOyAvLyDQn9C+0LTQutC70Y7Rh9Cw0LXQvCDRgdGC0LjQu9C4XG5cbnJlbmRlcig8QXBwIC8+LCBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgnYXBwJykhKTtcbiJdLCJmaWxlIjoiL2FwcC9zcmMvbWFpbi50c3gifQ==
