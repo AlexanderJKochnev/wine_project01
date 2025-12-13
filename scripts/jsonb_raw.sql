34ca022852c797a9ffc8d28207a2315320d7fe0f
Alexander <akochnev66@gmail.com>
2025-12-13 16:39:57 +0300

    jsonb script

--- scripts/jsonb_raw.sql
+++ scripts/jsonb_raw.sql
@@ -1,10 +1,10 @@
 -- создание столбца metadata_json -- все делаетсяя через alembic
-ALTER TABLE rawdatas ADD COLUMN metadata_json JSONB;
+-- ALTER TABLE rawdatas ADD COLUMN metadata_json JSONB;
 --  заполнение столбца metadata_json
 UPDATE rawdatas SET metadata_json = parsed_data::jsonb;
 
-ALTER TABLE rawdatas DROP COLUMN parsed_data;
+-- ALTER TABLE rawdatas DROP COLUMN parsed_data;
 
-CREATE INDEX rawdata_trigram_idx_combined ON rawdatas USING GIN ((metadata_json ->> 'name') gin_trgm_ops);
+-- CREATE INDEX rawdata_trigram_idx_combined ON rawdatas USING GIN ((metadata_json ->> 'name') gin_trgm_ops);
 
 -- ALTER TABLE rawdatas RENAME COLUMN metadata_json TO parsed_data;
