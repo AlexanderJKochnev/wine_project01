e09218f5b3457d860b933ae005f456d943fef159
Alexander <akochnev66@gmail.com>
2026-06-13 18:05:43 +0300

    prompt tbs
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/service.py
+++ app/support/vllm/service.py
@@ -100,7 +100,7 @@ class VLLMService:
         # собираем payload
         dataset = {'prompt': (Prompt, PromptRepository, 'role', 'system_prompt', prompt),
                    'writer': (WriterRule, WriterRuleRepository, 'name', 'prompt', writer),
-                   'proption': (Proption, ProptionRepository, 'preset', None, proption),
+                   'proption': (Proption, ProptionRepository, 'preset', None, proption)
                    # 'lang': (ISOLanguage, ISOLanguageRepository, 'iso_639_1', 'name_en', lang )
                    }
         payload: dict = {}
