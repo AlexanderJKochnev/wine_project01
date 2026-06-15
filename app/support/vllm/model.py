9387b195e8b4ce032f9882ae0bbe64c5c761d0bc
Alexander <akochnev66@gmail.com>
2026-06-15 14:53:55 +0300

    TranslaterawData ForeignOneToMany in drink
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/model.py
+++ app/support/vllm/model.py
@@ -34,8 +34,8 @@ class TranslateRawData(Base, BaseAt):
     proption: Mapped["Proption"] = relationship(back_populates=plural_name, cascade=cascade, lazy=lazy)
     subcategory_id: Mapped[int] = mapped_column(ForeignKey("subcategories.id"), nullable=False, index=True)
     subcategory: Mapped["Subcategory"] = relationship(back_populates=plural_name, cascade=cascade, lazy=lazy)
-    isolanguage_id: Mapped[int] = mapped_column(ForeignKey("isolanguages.id"), nullable = False, index = True)
-    isolanguage: Mapped["ISOLanguage"] = relationship(back_populates = plural_name, cascade = cascade, lazy = lazy)
+    # isolanguage_id: Mapped[int] = mapped_column(ForeignKey("isolanguages.id"), nullable = False, index = True)
+    # isolanguage: Mapped["ISOLanguage"] = relationship(back_populates = plural_name, cascade = cascade, lazy = lazy)
     result: Mapped[str] = mapped_column(Text)
     rate: Mapped[int_null_index]  # оценка перевода
 
@@ -45,7 +45,7 @@ class TranslateRawData(Base, BaseAt):
         return str(self.name) or ""
 
     __table_args__ = (UniqueConstraint(
-        'drink_id', 'lang_origin', 'prompt_id', 'writerrule_id', 'proption_id', 'isolanguage_id',
+        'drink_id', 'lang_origin', 'prompt_id', 'writerrule_id', 'proption_id', # 'isolanguage_id',
         'subcategory_id',
         name='uq_translate_raw_data_unique_combo'
     ),)
