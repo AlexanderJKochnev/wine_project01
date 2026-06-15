e909e0cd07f170f2e0da431568e8b9ceccbf49ce
Alexander <akochnev66@gmail.com>
2026-06-15 13:17:35 +0300

    TranslaterawData ForeignOneToMany in drink
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/migration/env.py
+++ app/migration/env.py
@@ -37,6 +37,7 @@ from app.support import Source
 from app.support.producer.model import Producer, ProducerTitle
 from app.support.parcel.model import Site, Parcel
 from app.support.tasting.model import Glassware, TastingNote, Scale, Body, BaseIngredient
+from app.support.vllm.model import TranslateRawData
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
