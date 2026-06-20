4c793eaab5391aa88a29ca4819a519779e8271b3
Alexander <akochnev66@gmail.com>
2026-06-20 20:41:05 +0300

    handbooks_translate service
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/service.py
+++ app/support/vllm/service.py
@@ -370,7 +370,14 @@ class VLLMService:
             # 2.
             source_field, target_field = f'name{origin}', f'name{dest}'
             # 3.
-
+            raw_sql = """
+            SELECT id, {orign}
+            FROM {handbook}
+            WHERE {dest} IS NULL
+            AND {origin} IS NOT NULL;
+            """
+            custom_sql = raw_sql.format(origin=origin, dest=dest, handbook=handbook)
+            print(f'{custom_sql=}')
             logger.warning(f'{system_prompt=}, \n\n {user_prompt=}, \n\n {source_field=}, \n\n {target_field=}, '
                            f'{handbook=}, \n\n {params}')
             return None
