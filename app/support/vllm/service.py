555d930187aeaae038084d6b7e46e722888c1144
Alexander <akochnev66@gmail.com>
2026-06-26 13:38:13 +0300

    TranslateHelper __add_transferhelper__
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/service.py
+++ app/support/vllm/service.py
@@ -486,6 +486,8 @@ class VLLMService:
         добавление ошибок в TranslateHelper
         """
         # 0. convert [(word, wrong, drow)] => [{'word': word, drow: [drow]}]
+        jprint(errors)
+        logger.critical('errors')
         result = defaultdict(list)
         for key, _, val in errors:
             result[key].append(val)
