b81b2f3e349f31a0463db7b9c0a1c92d41942bec
Alexander <akochnev66@gmail.com>
2026-06-26 19:35:30 +0300

    TranslateHelper __add_transferhelper__
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/model.py
+++ app/support/vllm/model.py
@@ -116,4 +116,5 @@ class TranslateHelper(Base, BaseAt):
     # shit True - подсказка переводчику, False - заменить на то что указано в drow
     shit: Mapped[bool] = mapped_column(Boolean, default=False, index=True, unique=False)
     # GIN-индекс для быстрого поиска внутри массива
-    __table_args__ = (Index("ix_translatehelper_gin", "drow", postgresql_using="gin"),)
+    __table_args__ = (Index("ix_translatehelper_gin", "drow", postgresql_using="gin",
+                            postgresql_ops={'drow': 'array_ops'}),)
