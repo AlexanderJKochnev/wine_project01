23914fa949d011e8b4f466449b45771da247831a
Alexander <akochnev66@gmail.com>
2026-06-27 12:19:21 +0300

    translate_service.py system_prompt
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/core/repositories/sqlalchemy_repository.py
+++ app/core/repositories/sqlalchemy_repository.py
@@ -152,6 +152,7 @@ class Repository(Background, metaclass=RepositoryMeta):
     @classmethod
     async def create(cls, obj: ModelType, model: ModelType, session: AsyncSession) -> ModelType:
         """ создание записи """
+        logger.warning(f'{obj=},   {type(obj)=}')
         await session.add(obj)
         await session.flush()
         await session.refresh(obj)
