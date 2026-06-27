35de34eb3d5264f0c7daea6761152e0762189951
Alexander <akochnev66@gmail.com>
2026-06-27 09:41:11 +0300

    translate_service.py system_prompt
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/model.py
+++ app/support/vllm/model.py
@@ -1,7 +1,7 @@
 # app/support/subcategory/model.py
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import List, TYPE_CHECKING
 
 from sqlalchemy import ARRAY, Boolean, Float, ForeignKey, Index, Integer, String, Text, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -112,7 +112,7 @@ class TranslateHelper(Base, BaseAt):
     # слово или фраза
     word: Mapped[str] = mapped_column(String, nullable=False, index=True, unique=True)
     # массив слов или фраз - возможные варианты перевода
-    drow: Mapped[set[str]] = mapped_column(SetArrayType, nullable=False, default=set, server_default='{}')
+    drow: Mapped[List[str]] = mapped_column(ARRAY(String), nullable=False)
     # shit True - подсказка переводчику, False - заменить на то что указано в drow
     shit: Mapped[bool] = mapped_column(Boolean, default=False, index=True, unique=False)
     # GIN-индекс для быстрого поиска внутри массива
@@ -127,6 +127,4 @@ class TranslateHelper(Base, BaseAt):
                             postgresql_ops={'drow': 'array_ops'}),)
 
     def __str__(self):
-        # переоопределять в особенных формах
-        # or "" на всякий случай если обязательное поле вдруг окажется необязательным и пустым
         return self.word or ""
