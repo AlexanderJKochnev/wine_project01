ab4b1e6b034e3deca78b69bb8cc56799cdcb7433
Alexander <akochnev66@gmail.com>
2026-06-27 07:37:57 +0300

    translate_service.py system_prompt
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/router.py
+++ app/support/vllm/router.py
@@ -298,7 +298,7 @@ class TranslateHelperRouter(BaseRouter):
                      replace: bool = Form(False, description='True - мусор для замены перед переводом, '
                                           'False - подсказка переводчику'),
                      session: AsyncSession = Depends(get_db)):
-        data = TranslateHelperCreate(word=word, drow=translate, shit=set(replace))
+        data = TranslateHelperCreate(word=word, drow=set(translate), shit=replace)
         return await super().create(data, session)
 
     async def patch(self, id: int, data: TranslateHelperUpdate, background_tasks: BackgroundTasks,
