9ab695a93e0afc1516a9d82b6568e042fcec4009
Alexander <akochnev66@gmail.com>
2026-06-27 12:28:02 +0300

    translate_service.py system_prompt
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/service.py
+++ app/support/vllm/service.py
@@ -590,9 +590,7 @@ class TranslateHelperService(ArrayService, Service):
         data_dict: dict = data.model_dump()
         for key in cls.array_fields:
             data_dict[key] = list(data_dict.get(key))
-        jprint(data_dict)
         obj = cls.model(**data_dict)
-        logger.critical(f'============================={obj}')
         result = await cls.repository.create(obj, cls.model, session)
-        logger.critical(f'{result=}')
+        await session.commit()
         return inst_dict(result)
