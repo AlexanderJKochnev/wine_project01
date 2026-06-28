2e19f1bd2faf40af69f034086fbb18ffd33fd9ca
Alexander <akochnev66@gmail.com>
2026-06-28 22:10:47 +0300

    AHO CORASICK implementaiton
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/service.py
+++ app/support/vllm/service.py
@@ -343,6 +343,7 @@ class VLLMService:
             result = await translation_service.real_batch(final_phrases, dataclass)
             # 4.1 evaluate
             evaluated: List[dict] = await translation_service.evaluate_translations_batch(result)
+            jprint(evaluated)
             # 4.2. extend error list
             # evaluated.get('errors') = [['Moutere', 'Моттера (Moutere)', 'Моттера']]
             err = [errors for item in evaluated if (errors := item.get('errors'))]
