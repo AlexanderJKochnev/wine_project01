0e3ed87162f4dde5ec42c349ed5093feeece56eb
Alexander <akochnev66@gmail.com>
2026-06-30 13:22:35 +0300

    разделение handbook_translate на методы
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/service.py
+++ app/support/vllm/service.py
@@ -535,7 +535,8 @@ class VLLMService:
         # evaluated.get('errors') = [['Moutere', 'Моттера (Moutere)', 'Моттера']]
         err = [errors for item in evaluated if (errors := item.get('errors'))]
         if err:
-            errors.extend(tuple(item for sublist in err for item in sublist))
+            print([item for sublist in err for item in sublist], '==========')
+            errors.extend([item for sublist in err for item in sublist])
         logger.success(f'оценено {len(evaluated)} записей. Результаты оценки ниже.')
         # 5. save to temporary file
         # 5.0. prepaire for save (score added)
@@ -557,7 +558,7 @@ class VLLMService:
         """
         jprint(errors)
         logger.warning('------------------errors--------------------')
-        errors_list_dict = [{'word': e[0], 'wrong': e[1], 'proposed': e[2]} for e in set(errors) if len(e) == 3]
+        errors_list_dict = [{'word': e[0], 'wrong': e[1], 'proposed': e[2]} for e in errors if len(e) == 3]
 
         rich_print(errors_list_dict, 'список ошибок')
         # 6.0 implementation to real database
