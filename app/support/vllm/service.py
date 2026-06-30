cc1334e32289fced06098c5dbf5ccf942968f1b5
Alexander <akochnev66@gmail.com>
2026-06-30 14:12:09 +0300

    разделение handbook_translate на методы
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/service.py
+++ app/support/vllm/service.py
@@ -556,6 +556,8 @@ class VLLMService:
             обработка результатов
         """
         if len(errors) > 0:
+            logger.warning('-----------------------------')
+            jprint(errors)
             errors_list_dict = [{'word': e[0], 'wrong': e[1], 'proposed': e[2]} for e in set(errors) if len(e) == 3]
             logger.warning(f'{len(errors)=},  {len(errors_list_dict)=}, ')
             rich_print(errors_list_dict, 'список ошибок')
