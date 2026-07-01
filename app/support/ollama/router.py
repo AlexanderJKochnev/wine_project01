c0471f76c601104e7644d1e9bcb4afd1c6dbe5fc
Alexander <akochnev66@gmail.com>
2026-07-01 10:47:25 +0300

    разделение handbook_translate на методы
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/ollama/router.py
+++ app/support/ollama/router.py
@@ -273,7 +273,7 @@ class WriterRuleRouter(BaseRouter):
     async def create(self,
                      name: str = Form(..., description='name'),
                      prompt: str = Form(..., description='промпт должен содержать {lang} {prase}',
-                                        json_schema_extra={"widget": "textarea"}),
+                                        json_schema_extra={"format": "textarea"}),
                      category: Categories = Form(..., description='категория к которой применен prompt'),
                      subcategory_ids: List[int] = Form(..., description='id субкатегорий'),
                      active: bool = Form(True, description='активировано'),
