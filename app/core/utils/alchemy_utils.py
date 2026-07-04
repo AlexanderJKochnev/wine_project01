decd6f27c46ec5bd5200efdf5c77feded312b1c1
Alexander <akochnev66@gmail.com>
2026-07-04 18:06:59 +0300

    backgroundtask for update translate after change translatehelper
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/core/utils/alchemy_utils.py
+++ app/core/utils/alchemy_utils.py
@@ -216,9 +216,9 @@ def get_models_with_columns(lang_suff: str) -> List[ModelType]:
         (получать имя через .__name__)
         МАГИЯ - ВЫЗОВ СТРОКИ НАПРЯМУЮ ГДЕ ЛИБО НЕ ДАЙТ РЕЗУЛЬТАТА - ТОЛЬКО ЧЕРЕЗ get_models
     """
-    return [(cls, column) for cls in Base.registry._class_registry.values()
+    return [(cls, column.name) for cls in Base.registry._class_registry.values()
             if isinstance(cls, type) and hasattr(cls, '__table__') for column in cls.__table__.columns
-           ]
+            ]
 
 
 def get_model_by_tablename(tablename: str):
