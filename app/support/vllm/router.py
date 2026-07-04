64d1066e95872b94486d3a28ccc4291559f60f30
Alexander <akochnev66@gmail.com>
2026-07-04 16:58:21 +0300

    backgroundtask for update translate after change translatehelper
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/router.py
+++ app/support/vllm/router.py
@@ -222,14 +222,14 @@ class VllmRouter(LightRouter):
     async def test(self, session: AsyncSession = Depends(get_db)):
         # w = [cls for cls in Base.registry._class_registry.values()
         #      if isinstance(cls, type) and hasattr(cls, '__table__')]
-        w = (mapper.class_ for mapper in Base.registry.mappers)
-        for i in w:
-            print('--', i)
-        print('-------------------------------------------------------')
         response = get_models()
         res = list(response)
         print(res)
         result = {n: x.__name__ for n, x in enumerate(res)}
+        w = (mapper.class_ for mapper in Base.registry.mappers)
+        for i in w:
+            print('--', i)
+        print('-------------------------------------------------------')
         return result
 
     async def drink_translate(self, background_tasks: BackgroundTasks,
