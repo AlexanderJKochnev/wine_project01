b1232e40245250ed663fc659fcccb43f3abccf1f
Alexander <akochnev66@gmail.com>
2026-07-04 18:16:04 +0300

    backgroundtask for update translate after change translatehelper
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/support/vllm/router.py
+++ app/support/vllm/router.py
@@ -226,7 +226,7 @@ class VllmRouter(LightRouter):
         # res = response
         # result = {n: x.__name__ for n, x in enumerate(res)}
         response = get_models_with_columns('ru')
-        jprint(response)
+        print('========================', type(response), '==========================')
         return response
 
     async def drink_translate(self, background_tasks: BackgroundTasks,
