18129ab7487ce4c1dbe7863531b82a209522003f
Alexander <akochnev66@gmail.com>
2026-07-23 21:48:41 +0300

    remove minhash
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/core/config/database/redis_async.py
+++ app/core/config/database/redis_async.py
@@ -2,7 +2,6 @@
 
 from typing import Optional
 from redis.asyncio import ConnectionPool, Redis as AsyncRedis
-from datasketch.aio import AsyncMinHashLSH  # Нативный асинхронный драйвер
 
 from app.core.config.project_config import settings
 from loguru import logger
@@ -16,7 +15,6 @@ class RedisManager:
         self._threshold: float = settings.SIMILARITY_THRESHOLD
         self._num_perm: int = settings.NUM_PERM
         self.pool: Optional[ConnectionPool] = None
-        self.lsh_driver: Optional[AsyncMinHashLSH] = None
 
     async def connect(self):
         """Асинхронная инициализация пула и проверка связи"""
@@ -37,35 +35,6 @@ class RedisManager:
             logger.error(f"❌ Redis connection failed: {e}")
             raise e
 
-    async def init_lsh_driver(self) -> None:
-        """
-        Асинхронная инициализация тяжелого драйвера поиска.
-        Использует aioredis/redis.asyncio бэкенд из общего пула.
-        """
-        try:
-            logger.info("⏳ Инициализация асинхронного драйвера AsyncMinHashLSH...")
-
-            # Получаем асинхронного клиента из нашего пула
-            # shared_async_client = self.get_async_client()
-
-            # Конфигурация для асинхронного бэкенда datasketch
-            storage_config = {"type": "aioredis",
-                              "redis": {"host": self._host, "port": self._port, "password": self._password, "db": 0,
-                                        "decode_responses": False}}
-
-            self.lsh_driver = await AsyncMinHashLSH(
-                threshold=self._threshold, num_perm=self._num_perm, storage_config=storage_config
-            )
-            logger.info("✅ Redis Manager: Драйвер AsyncMinHashLSH успешно развернут")
-        except Exception as e:
-            logger.error(f"⚠️ Ошибка инициализации AsyncMinHashLSH: {e}. Поиск отключен.")
-            self.lsh_driver = None
-
-    def disable_lsh_driver(self) -> None:
-        """Метод для динамического отключения поиска 'на лету' без остановки Redis."""
-        self.lsh_driver = None
-        logger.warning("🛑 Драйвер MinHashLSH отключен. Поисковые функции деактивированы.")
-
     async def disconnect(self):
         """Асинхронное закрытие всех соединений"""
         if self.pool:
@@ -77,8 +46,3 @@ class RedisManager:
         if not self.pool:
             raise RuntimeError("Redis pool is not initialized")
         return AsyncRedis(connection_pool=self.pool)
-
-    def get_lsh_driver(self) -> AsyncMinHashLSH:
-        if not self.lsh_driver:
-            raise RuntimeError("Драйвер AsyncMinHashLSH в данный момент отключен.")
-        return self.lsh_driver
