f3bf283d2110edfc012a4525cab1e6fc64f15492
Alexander <akochnev66@gmail.com>
2026-07-25 01:08:49 +0300

    try starlette-admin
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/auth.py
+++ app/admin/auth.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
+from starlette.responses import Response
 from starlette_admin.auth import AuthProvider
 from starlette_admin.exceptions import LoginFailed
 
@@ -12,24 +13,24 @@ from app.core.config.database.db_async import get_db
 
 
 class AdminAuthProvider(AuthProvider):
-    async def login(self, request: Request) -> Request:
+    async def login(
+        self, username: str, password: str,
+        remember_me: bool, request: Request, response: Response,
+    ) -> Response:
         # Получаем данные из стандартной HTML-формы админки
         form = await request.form()
         username = form.get("username")
         password = form.get("password")
-        
         repo = UserRepository
-        session = get_db()  # Убедитесь, что это асинхронная сессия
-        
+        session = get_db()
         user = await repo.authenticate(username, password, session)
         if user and user.is_superuser:
             user_data = {"username": username, "role": "superuser"}
             # Записываем данные в сессию FastAPI
             request.session.update({"admin_user": user_data})
-            
             # Возвращаем request, это сигнализирует starlette-admin об успешном входе
-            return request
-        
+            return response
+
         raise LoginFailed("Неверное имя пользователя или пароль или прав недостаточно")
     
     async def logout(self, request: Request) -> bool:
