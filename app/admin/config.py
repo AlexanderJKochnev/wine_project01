55920a907b96b21c9741e8263a527af8401056b5
Alexander <akochnev66@gmail.com>
2026-07-24 21:03:20 +0300

    try starlette-admin
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/config.py
+++ app/admin/config.py
@@ -5,12 +5,21 @@ starlette-admin
 
 from fastapi import FastAPI
 from starlette_admin.contrib.sqla import Admin
-
+from sqlalchemy.engine import make_url
+from sqlalchemy.future import Engine
+from sqlalchemy.pool import NullPool
 from app.admin.auth import AdminAuthProvider
 from app.admin.views import UserAdminView
 from app.auth.models import User
 
+
+# 🛠 СОЗДАЕМ ЗАГЛУШКУ ДВИЖКА (чтобы избежать ошибки при старте)
+# Он не делает сетевых запросов и нужен только для инициализации класса Admin
+mock_url = make_url("postgresql+asyncpg://mock_user:mock_pass@localhost/mock_db")
+mock_engine = Engine(pool=NullPool(), url=mock_url, dialect=mock_url.get_dialect()())
+
 admin = Admin(
+    engine=mock_engine,
     title="Управление системой",
     base_url="/admin",
     auth_provider=AdminAuthProvider()
