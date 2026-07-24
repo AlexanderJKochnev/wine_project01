12f59111c11807d4da2bc2a3fc02d4990417ef7b
Alexander <akochnev66@gmail.com>
2026-07-24 11:00:55 +0300

    try fastapi_amis_admin auth added
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/auth/models.py
+++ app/auth/models.py
@@ -5,6 +5,7 @@ from sqlalchemy import DateTime, String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from app.core.models.base_model import Base
 from typing import Optional
+from fastapi_user_auth.auth.models import Role, CasbinRule, LoginHistory  # NOQA: F401
 
 
 class User(Base):
