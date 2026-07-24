a1fd42945847ed40821dad26ab85363402caac9a
Alexander <akochnev66@gmail.com>
2026-07-24 09:13:57 +0300

    try fastapi_amis_admin auth added
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,5 +1,7 @@
 # app/auth/models.py
-from sqlalchemy import String, Boolean
+from datetime import datetime
+
+from sqlalchemy import DateTime, String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from app.core.models.base_model import Base
 from typing import Optional
@@ -13,6 +15,7 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column(String(255))
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    delete_time: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True, default=None)
 
     def __repr__(self):
         return f"<User(id={self.id}, username='{self.username}')>"
