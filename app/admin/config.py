a1b6cb788093348db6847e8daaa2443a881ac7e1
Alexander <akochnev66@gmail.com>
2026-07-25 09:43:40 +0300

    rename static to statics
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/config.py
+++ app/admin/config.py
@@ -18,7 +18,7 @@ def setup_starlette_admin(app: FastAPI, async_engine) -> Admin:
         engine=async_engine,  # Передаем ваш асинхронный engine
         title="Управление системой", base_url="/admin",
         templates_dir="templates",  # Указывает на вашу папку с измененным layout.html
-        statics_dir="static",  # Указывает на папку со скачанными CSS/JS файлами
+        statics_dir="statics",  # Указывает на папку со скачанными CSS/JS файлами
         auth_provider=AdminAuthProvider(),
         route_name="admin"
     )
