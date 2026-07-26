93bc413494fb021467a501291bdb07e8a63d9f22
Alexander <akochnev66@gmail.com>
2026-07-26 15:04:59 +0300

    add views with back populate
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/config.py
+++ app/admin/config.py
@@ -108,6 +108,7 @@ def setup_starlette_admin(app: FastAPI, async_engine) -> Admin:
     # admin.add_view(SomeView(TranslateHelper, identity="translatehelper", label="Словарь"))
     admin.add_view(HandbookView(Category, identity="category", label="Категории"))
     admin.add_view(TestView(Category, identity="category", label="Категории"))
+    admin.add_view(TestView(TranslateHelper, identity="translatehelper", label="TranslateHelper"))
     admin.mount_to(app)
 
     return admin
