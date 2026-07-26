598a68d3a0e829ddabc4ed8331435952e06b375d
Alexander <akochnev66@gmail.com>
2026-07-26 06:20:52 +0300

    add views with back populate
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,5 +1,6 @@
 # app.admin.views.py
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import joinedload, selectinload
+from starlette.requests import Request
 from starlette_admin.contrib.sqla import ModelView
 from starlette_admin.fields import BooleanField, DateTimeField, IntegerField, PasswordField, StringField
 
@@ -29,7 +30,15 @@ class SomeView(ModelView):
 
 class CategoryView(ModelView):
     # Переопределяем базовое выражение SELECT для этой таблицы
-    def select_expr(self, request):
-        # Берем стандартный SELECT запрос админки и добавляем жадную загрузку связи
-        # Замените 'author' на точное имя вашего поля с back_populates
-        return super().select_expr(request).options(selectinload(self.model.subcategories))
+    def get_list_query(self, request: Request):
+        # 1. Берем оригинальный select-запрос админки
+        query = super().get_list_query(request)
+
+        # 2. Добавляем joinedload для поля с back_populates
+        # ВНИМАНИЕ: Замените self.model.author на ваш атрибут связи
+        return query.options(joinedload(self.model.subcategories))
+
+    # Для корректного подсчета количества записей переопределяем и count-запрос
+    def get_count_query(self, request: Request):
+        query = super().get_count_query(request)
+        return query.options(joinedload(self.model.subcategories))
