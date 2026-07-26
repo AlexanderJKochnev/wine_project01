89a03f71aecf88c71c059faf038a921f4175345e
Alexander <akochnev66@gmail.com>
2026-07-26 05:55:30 +0300

    add views to admin TBS Принудительно объединяем кастомный и оригинальный загрузчики
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/config.py
+++ app/admin/config.py
@@ -4,6 +4,7 @@ starlette-admin
 """
 
 from fastapi import FastAPI
+from jinja2 import ChoiceLoader, FileSystemLoader
 from starlette_admin.contrib.sqla import Admin
 
 from app.admin.auth import AdminAuthProvider
@@ -24,6 +25,14 @@ def setup_starlette_admin(app: FastAPI, async_engine) -> Admin:
         auth_provider=AdminAuthProvider(),
         route_name="admin"
     )
+
+    # 2. ЖЕСТКОЕ РЕШЕНИЕ: Принудительно объединяем кастомный и оригинальный загрузчики
+    # Это заставит Jinja2 ВСЕГДА проверять вашу папку первой, даже при 404 и logout
+    original_loader = admin.templates.env.loader
+    custom_loader = FileSystemLoader("templates")
+
+    # ChoiceLoader сначала ищет файл у вас, а если не находит — берет дефолтный из пакета
+    admin.templates.env.loader = ChoiceLoader([custom_loader, original_loader])
 
     admin.add_view(UserAdminView(User, identity="user", label="Пользователи"))
     admin.add_view(SomeView(TranslateHelper, identity="translatehelper", label="Словарь"))
