311866d3103ddaaed46bbd92f2cd9c46d1cc9587
Alexander <akochnev66@gmail.com>
2026-07-26 12:52:41 +0300

    add views with back populate
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/config.py
+++ app/admin/config.py
@@ -21,7 +21,7 @@ def setup_starlette_admin(app: FastAPI, async_engine) -> Admin:
         engine=async_engine,  # Передаем ваш асинхронный engine
         title="Админ панель", base_url="/panel",
         templates_dir="app/templates",  # Указывает на вашу папку с измененным layout.html
-        statics_dir="app/statics",  # Указывает на папку со скачанными CSS/JS файлами
+        statics_dir="statics",  # Указывает на папку со скачанными CSS/JS файлами
         auth_provider=AdminAuthProvider(),
         route_name="admin"
     )
