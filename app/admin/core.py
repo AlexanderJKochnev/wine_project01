921e9831bb70814c96d23084c06ae41d4c063ee6
Alexander <akochnev66@gmail.com>
2026-07-26 12:39:51 +0300

    add views with back populate
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/core.py
+++ app/admin/core.py
@@ -2,10 +2,60 @@
 """
 core component of views
 """
-from starlette_admin import IntegerField, StringField, TextAreaField
+from starlette_admin import DateTimeField, IntegerField, StringField, TextAreaField
 
-# FIELDS
-id = IntegerField("id", label="ID", exclude_from_create=True)
-name = StringField("name", label="Name", required=True, searchable=True, orderable=True)
-name_ru = StringField("name_ru", label="Наименование", required=False, searchable=True, orderable=True)
-description = TextAreaField("description", label="Description", required=False, exclude_from_list=True)
+from app.core.config.project_config import settings
+
+
+class FieldsCore():
+    """
+    заготовки для поле AdminViews
+    HasOne(
+            "user",                    # Точное имя атрибута relationship с back_populates
+            identity="user",           # Идентификатор связанной модели в админке
+            label="Пользователь",
+        ),
+    """
+    def __init__(self):
+        # локализованные поля ('name', 'description', ...)
+        self.fields_localized: tuple = settings.FIELDS_LOCALIZED
+        # языковые суффиксы ('', '_ru', ...)
+        self.langs: tuple = settings.lang_suffixes
+        # поля типа  TextAreaField
+        self.textareafields: tuple = ('description',)
+        self.id = IntegerField("id", label="ID", exclude_from_create=True)
+        self.created_at = DateTimeField("created_at", label="Дата создания", exclude_from_list=True,
+                                        exclude_from_create=True, exclude_from_edit=True,
+                                        orderable=True)
+        self.update_at = DateTimeField(
+            "created_at", label="Дата обновления", exclude_from_list=True, exclude_from_create=True,
+            exclude_from_edit=True, orderable=True
+        )
+        self.localized_fields = self.localized_field_generator()
+
+    def localized_field_generator(self) -> list:
+        """
+            возвращает список локлизованных полей
+        """
+        strfields: list = []
+        textfields: list = []
+        for lang in self.langs:
+            required = True if lang == '' else False
+            for field in self.fields_localized:
+                if field in self.textareafields:
+                    textfields.append(TextAreaField(field, label=field.capitalize(),
+                                                    required=False, exclude_from_list=True))
+                else:
+                    strfields.append(StringField(field, label=field.capitalize(),
+                                                 required=required, searchable=True, orderable=True,
+                                                 ))
+        return strfields + textfields
+
+    def __simple__(self):
+        self.id = IntegerField("id", label="ID", exclude_from_create=True)
+
+    def __call__(self, *args, **kwargs):
+        result: list = [self.id]
+        result.extend(self.localized_fields)
+        result.append(self.created_at)
+        result.append(self.update_at)
--- app/admin/views.py
+++ app/admin/views.py
@@ -4,6 +4,8 @@ from starlette.requests import Request
 from starlette_admin.contrib.sqla import ModelView
 from starlette_admin.fields import BooleanField, DateTimeField, IntegerField, PasswordField, StringField
 
+from app.admin.core import FieldsCore
+
 
 class UserAdminView(ModelView):
     # Явно декларируем поля для форм и таблиц с правильными типами из starlette_admin.fields
@@ -24,21 +26,9 @@ class UserAdminView(ModelView):
     filterable_fields = ["is_active", "is_superuser"]
 
 
-class SomeView(ModelView):
+core_fields = FieldsCore()
+
+
+class HandbookView(ModelView):
     pk_attr = "id"
-
-
-class CategoryView(ModelView):
-    # Переопределяем базовое выражение SELECT для этой таблицы
-    def get_list_query(self, request: Request):
-        # 1. Берем оригинальный select-запрос админки
-        query = super().get_list_query(request)
-
-        # 2. Добавляем joinedload для поля с back_populates
-        # ВНИМАНИЕ: Замените self.model.author на ваш атрибут связи
-        return query.options(joinedload(self.model.subcategories))
-
-    # Для корректного подсчета количества записей переопределяем и count-запрос
-    def get_count_query(self, request: Request):
-        query = super().get_count_query(request)
-        return query.options(joinedload(self.model.subcategories))
+    fields = core_fields()
