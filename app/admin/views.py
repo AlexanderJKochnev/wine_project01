e911e0bc5c330cb8b54baf95f7ba4fd4406bb31a
Alexander <akochnev66@gmail.com>
2026-07-27 05:59:19 +0300

    CategoryView
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/views.py
+++ app/admin/views.py
@@ -52,10 +52,10 @@ class SubcategoryView(ModelView):
 class CategoryView(ModelView):
     pk_attr = "id"
     fields = core_fields()
-    fields.insert(1, ColorField('color', label='Color',
+    fields.insert(1, CleanColorField(  # 'color', label='Color',
                                 # exclude_from_list=True,
                                 # render_function_key="renderColor",
-                                display_template="displays/color.html",
+                                # display_template="displays/color.html",
                                 ))
 
 
