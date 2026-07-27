351d12cd704d80990f54eed3f86ae0eb78c94300
Alexander <akochnev66@gmail.com>
2026-07-27 18:30:43 +0300

    DrinkView
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/core.py
+++ app/admin/core.py
@@ -31,7 +31,7 @@ class HandBooksFieldsCore():
         # языковые суффиксы ('', '_ru', ...)
         self.langs: tuple = settings.lang_suffixes
         # поля типа  TextAreaField
-        self.textareafields: tuple = ('description', 'recomendation', 'madeof')
+        self.textareafields: tuple = ('description', 'recommendation', 'madeof')
         self.id = IntegerField("id", label="ID", exclude_from_create=True)
         self.created_at = DateTimeField("created_at", label="Дата создания", exclude_from_list=True,
                                         exclude_from_create=True, exclude_from_edit=True,
