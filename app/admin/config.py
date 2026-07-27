85e6e84533f0dc5aca153a04f2e3fdffca2b018f
Alexander <akochnev66@gmail.com>
2026-07-27 09:40:21 +0300

    Geo Views
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/config.py
+++ app/admin/config.py
@@ -15,7 +15,8 @@ from starlette_admin.i18n import (get_locale, get_locale_display_name, get_timez
 from starlette_admin.views import BaseModelView, DropDown, Link
 
 from app.admin.auth import AdminAuthProvider
-from app.admin.views import CategoryView, CountryView, SubcategoryView, TestView, UserAdminView
+from app.admin.views import CategoryView, CountryView, RegionView, SubcategoryView, SubregionView, TestView, \
+    UserAdminView
 from app.auth.models import User
 from app.support import Category, Country, Region, Subcategory, Subregion
 from app.support.vllm.model import TranslateHelper
@@ -101,8 +102,8 @@ def setup_starlette_admin(app: FastAPI, async_engine) -> Admin:
     admin.add_view(CategoryView(Category, identity="category", label="Категории"))
     admin.add_view(SubcategoryView(Subcategory, identity="subcategory", label="Подкатегории"))
     admin.add_view(CountryView(Country, identity='country', label='Страны'))
-    admin.add_view(CountryView(Region, identity='region', label='Регионы'))
-    admin.add_view(CountryView(Subregion, identity='subregion', label='Cубрегионы'))
+    admin.add_view(RegionView(Region, identity='region', label='Регионы'))
+    admin.add_view(SubregionView(Subregion, identity='subregion', label='Cубрегионы'))
     admin.add_view(TestView(TranslateHelper, identity="translatehelper", label="TranslateHelper"))
     admin.mount_to(app)
 
