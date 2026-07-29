f298f3c8f9d20303646fd651c0b87331e29e6073
Alexander <akochnev66@gmail.com>
2026-07-29 11:08:17 +0300

    sortable_fields HasOne
    Signed-off-by: Alexander <akochnev66@gmail.com>

--- app/admin/views.py
+++ app/admin/views.py
@@ -58,8 +58,8 @@ class SubcategoryView(ModelView):
         )
     )
     fields.insert(3, HasMany('drinks', identity='drink', exclude_from_list=True, exclude_from_detail=True))
-    sortable_field = ["name", "category"]
-    sortable_field_mapping = {"category": Category.name,  # Sort by the age of the related user
+    sortable_fields = ["name", "category"]
+    sortable_field_mapping = {"category": Category.name,
                               }
 
     async def find_by_pks(self, request: Request, pks: List[Any]) -> List[Any]:
